cd9891b4cc2e28983173bace008cf9f3285823de
RobynSaul <robyn.saul@gmail.com>
2023-10-29 21:40:32 +0000

    Testing integration of HTML and JS
    Files are synched, but I clearly don't quite know how to get the one to affect the other. Need to get a main function that runs all the smaller functions linked to the start button but I'm too tired to figure it out now.

--- main.js
+++ main.js
@@ -1,31 +1,40 @@
 const divClass = document.getElementsByClassName("square");
-// let divNo = 1;
+const Go = document.getElementById("StartBtn");
 
 //Random number generator - tested, generates a random number from 1 to 6 - not sure about how it's passing the number out?
-function randomSix (divNo) {
- return Math.floor(Math.random() * 6) + 1;
-//  console.log(DivNo)
+function randomSix() {
+    let divNo = Math.floor(Math.random() * 6) + 1;
+    console.log(divNo); //remove later
+    }
+
+//Deactivate all the divs: untested
+function deactivate() {
+divClass.style.backgroundColor = 'blue';
+divClass.removeEventListener('click', function(){});//the function needs to be the same as the function name called in Active - this might also need to be applied to divActive after click
 }
 
+
 //Activate one div - untested and needs the click functionality added
-function activate () {
-    randomSix
+function activate() {
+    randomSix();
     const divActive = document.getElementById(divNo);
     divActive.style.backgroundColor = 'red';
-    divActive.addEventListener('click', function(){
+    divActive.addEventListener("click", function(){ //add function here
         
     });
 }
 
-
 //Timer
-
-let countdown = 60
-
-while (countdown > 0){
- console.log(countdown)
- countdown--
+function timer() {
+    let countdown = 60
+    while (countdown > 0){
+     console.log(countdown);
+     countdown--;
+    }
+    console.log("Game over");     
 }
 
-console.log("Game over")
-   
+//Programm to run here:
+//alert("Success") - testing js linked to html, it is... but the below doesn't work
+// startBtn.addEventListener("click", activate());
+Go.addEventListener("click", function(){alert("go")}); //test code
