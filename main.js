1388d62c86ff66c362d888263ba539666332facc
RobynSaul <robyn.saul@gmail.com>
2023-10-29 20:20:46 +0000

    Added the outlines of the activate and deactivate functions
    Neither have been tested...

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
-const divClass = document.getElementsByClassName("square")
-const divActive = document.getElementById()
+const divClass = document.getElementsByClassName("square");
+// let divNo = 1;
 
-//Randon number generator
-func randomSix (divno) {
+//Random number generator - tested, generates a random number from 1 to 6 - not sure about how it's passing the number out?
+function randomSix (divNo) {
  return Math.floor(Math.random() * 6) + 1;
 //  console.log(DivNo)
 }
+
+//Activate one div - untested and needs the click functionality added
+function activate () {
+    randomSix
+    const divActive = document.getElementById(divNo);
+    divActive.style.backgroundColor = 'red';
+    divActive.addEventListener('click', function(){
+        
+    });
+}
+
 
 //Timer
 
